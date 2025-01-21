37a43f37c317155a712a279598e3cb642132f619
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-21 21:56:35 +0545

    user model added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema({
 // Pre-save hook to hash password before saving to database
 userSchema.pre("save",async function (next)
 {
-    if(!this.isModified("Password")) return next();
+    if(!this.isModified("password")) return next();
 
     try{
         const salt = await bcrypt.genSalt(10);
