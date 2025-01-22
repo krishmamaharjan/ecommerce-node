e4669c0cbbe792100f354b1ead56eddc1c108b80
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-22 22:01:02 +0545

    cart routes added

---
+++ backend/routes/cart.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+    addToCart,
+    removeAllFromCart,
+    getCartProducts,
+    updateQuantity
+    } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+const router = express.Router();
+
+router.post("/",protectRoute,getCartProducts);
+router.post("/",protectRoute,addToCart);
+router.delete("/",protectRoute,removeAllFromCart);
+router.put("/:id",protectRoute,updateQuantity);
+
+export default router;
