1fd0ca57b28a4109c695eaa48e3683e3ee6eb677
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-30 21:32:57 +0545

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
-function App(){
-    return <div> hello world </div>;
+import {Route, Routes} from 'react-router-dom';
+function App()
+{
+  return(
+   <div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+
+      
+    </Routes>
+    </div>
+  )
 }
 
 export default App;
